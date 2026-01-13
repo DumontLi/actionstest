a211c6953758a5eb90282e43236f32dcbbe4d798
Dumont Lienou <dumson84@yahoo.fr>
2026-01-13 18:23:19 +0000

    commit with creds

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_vpc" "actions" {
-   cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name = "Class32"
-    Team = "DevOps"
+    Name        = "Class32"
+    Team        = "DevOps"
     Environment = "Prod"
   }
 }
